29619d4f911171147a635c5b2b50b566f8d53c83
Jofreem <jfreem06@tufts.edu>
2023-10-16 21:50:54 -0400

    Adding in SubcategoryParser, tests, boilerplate code

---
+++ src/transformers/line_item_transformer.py
@@ -0,0 +1,11 @@
+from src.models.line_item import LineItem
+
+
+class LineItemTransformer:
+
+    def __init__(self):
+        pass
+
+    def transform(self) -> LineItem:
+        # return LineItem()
+        pass
