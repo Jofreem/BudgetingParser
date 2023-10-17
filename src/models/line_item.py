2b7dbbe032bd0107e4f2e54c973dd97332e89e37
Jofreem <jfreem06@tufts.edu>
2023-10-16 22:38:55 -0400

    BP-2 Adding LineItem, small SubcategoryParser changes

--- src/models/line_item.py
+++ src/models/line_item.py
@@ -3,21 +3,29 @@ from datetime import datetime
 from decimal import Decimal
 from typing import Optional
 
+from src.transformers.subcategory_parser import SubcategoryParser
+
 @dataclass
 class LineItem:
     """
     Object that holds the attributes and data required for an Expense
     """
 
-    date: datetime
-    expense: str
-    amount: Decimal
+    transaction_date: datetime
+    expense_description: str
     category: str
     source: str
     raw_subcategory: str
+    debit_amount: Optional[Decimal] = None
+    credit_amount: Optional[Decimal] = None
     true_subcategory: Optional[str] = None
 
+    def __post_init__(self):
+        if self.debit_amount is None and self.credit_amount is None:
+            raise ValueError("Either 'Debit' or 'Credit' must be set, but not both.")
+        elif self.debit_amount is not None and self.credit_amount is not None:
+            raise ValueError("Both 'Debit' and 'Credit' cannot be set at the same time.")
+
     def parse_subcategory(self) -> None:
-        pass
-        #self.true_subcategory = SubcategoryParser(self.subcategory)
+        self.true_subcategory = SubcategoryParser().transform(self.raw_subcategory, self.expense_description)
 
